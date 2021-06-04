f9669be777d1ba58b6530fa0f518ca27a47d560a
harkiratsm <71957674+harkiratsm@users.noreply.github.com>
2021-06-04 13:08:29 +0530

    updates

--- src/components/youtube.js
+++ src/components/youtube.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 // api-key1=AIzaSyBQS3Ly8N9Yo8Ca5aEezmoSRfkf2szKJy8
 // api-key2=AIzaSyDmzTPb2SpBSmNFDm_Dd5DuXbljJ2c77Ek
 // api-key3=AIzaSyCYjBla1ZAjpSyeLuiAYc7roXxcTUgyQ24
-const KEY='AIzaSyCYjBla1ZAjpSyeLuiAYc7roXxcTUgyQ24';
+const KEY='AIzaSyBQS3Ly8N9Yo8Ca5aEezmoSRfkf2szKJy8';
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3/',
     params: {
